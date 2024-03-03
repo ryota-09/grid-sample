e89eee7f08ba671c857502df49359fb2f897c95d
ryota-09 <n.ryo950911@gmail.com>
2024-03-04 08:16:19 +0900

    グリッドラインの設定

--- components/base/index.tsx
+++ components/base/index.tsx
@@ -1,8 +1,15 @@
+import "./base.css"
 const Base = () => {
-    return (
-        <div>
-            <h1>Base</h1>
-        </div>
-    )
+  return (
+    <div>
+      <div className="grid">
+        <h2 className="item1 boots-logo">Boots Fashion</h2>
+        <img className="item2 img-fill" src="/boots.jpg" alt="" width="1980" height="1512" />
+        <img className="item3 img-fill" src="/shirts.jpg" alt="" width="1320" height="1980" />
+        <img className="item4 img-fill" src="/shop.jpg" alt="" width="1600" height="1144" />
+        <img className="item5 img-fill" src="/bag.jpg" alt="" width="1320" height="1980" />
+      </div>
+    </div>
+  )
 }
 export default Base
