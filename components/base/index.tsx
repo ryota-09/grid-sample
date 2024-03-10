52640e68078756d0203337990983505335710290
ryota-09 <n.ryo950911@gmail.com>
2024-03-11 08:25:48 +0900

    重複するケース

--- components/base/index.tsx
+++ components/base/index.tsx
@@ -1,5 +1,6 @@
 // import "./base.css"
-import "./track-column.css"
+// import "./track-column.css"
+import "./double.css"
 const Base = () => {
   return (
     <div>
@@ -9,6 +10,9 @@ const Base = () => {
         <img className="item3 img-fill" src="/shirts.jpg" alt="" width="1320" height="1980" />
         <img className="item4 img-fill" src="/shop.jpg" alt="" width="1600" height="1144" />
         <img className="item5 img-fill" src="/bag.jpg" alt="" width="1320" height="1980" />
+        <img className="item6 img-fill" src="/socks.jpg" alt="" width="1920" height="1080" />
+        <img className="item7 img-fill" src="/watch.jpg" alt="" width="1920" height="1080" />
+        <img className="item8 img-fill" src="/shelf.jpg" alt="" width="1920" height="1080" />
       </div>
     </div>
   )
